bf0aaa1415f28b253873030255bb723693d9aeda
Yakhnitsa <transpmailbox@gmail.com>
2020-06-23 20:48:40 +0300

    Add dropdown menu items with locale

--- src/main/java/com/yurets_y/mydevelopercv/controller/MainController.java
+++ src/main/java/com/yurets_y/mydevelopercv/controller/MainController.java
@@ -3,6 +3,7 @@ package com.yurets_y.mydevelopercv.controller;
 import com.yurets_y.mydevelopercv.entities.User;
 import com.yurets_y.mydevelopercv.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,12 +13,16 @@ public class MainController {
 
     private UserService userService;
 
+    @Value("${application.controller.sourcecode.github-link}")
+    private String githubLink;
+
     @GetMapping
     public String mainPage(
         Model model
     ){
         User user = userService.getUser();
         model.addAttribute("user",user);
+        model.addAttribute("githubLink",githubLink);
         return "index.html";
     }
 
